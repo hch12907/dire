69ee7d79f7efd21bc5aa8ad5a8265c9fa42aba3b
hch12907 <haochengho12907@gmail.com>
2018-06-02 21:10:58 +0800

    Renaming

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate dirs;
+extern crate dire;
 
 fn main() {
-    let dirs = dirs::Directories::with_prefix("foobar", "Foobar").unwrap();
+    let dire = dire::Directories::with_prefix("foobar", "Foobar").unwrap();
     println!("data_home:   {:?}", dirs.data_home());
     println!("config_home: {:?}", dirs.config_home());
     println!("cache_home:  {:?}", dirs.cache_home());
